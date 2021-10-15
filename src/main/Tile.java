525a79f58952a69c6561c80bb23784b6b0247e2d
akansha313 <aka.singh@mail.utoronto.ca>
2021-10-15 02:15:06 -0400

    Tile class updates to run the game, added print methods and improved other board methods (Akansha, Jun, Koji, Chris)

--- src/main/Tile.java
+++ src/main/Tile.java
@@ -10,11 +10,11 @@ import java.util.Scanner;
 public class Tile {
 
     private int key;
-    // private String theme
+    // TO-DO private String theme
 
 
     // constructor for Tile object
-    public Tile(int key) {         // add String theme as a parameter
+    public Tile(int key) {         // add String "theme" as a parameter for phase 1/2
         this.key = key;
     }
 
@@ -27,57 +27,73 @@ public class Tile {
     }
 
     public ArrayList<Tile> createTileList () {
-        ArrayList<Tile> tileList = new ArrayList<Tile>();       // check syntax
+        ArrayList<Tile> tileList = new ArrayList<Tile>();
         for (int i = 0; i <= 6; i++) {          // loops 6 times, this value is used for keys
-            for (int j = 0; j <= 2; j++) {
-                Tile newTile = new Tile(i);         // creates 2 tiles with consecutive keys
-                tileList.add(newTile);
+            for (int j = 0; j <= 2; j++) {      // each key is used twice
+                Tile newTile = new Tile(i);     // creates 2 tiles with consecutive keys
+                tileList.add(newTile);          // adds newly created tile to the list of tiles
             }
         }
-        // randomize tiles
-        Collections.shuffle(tileList);
+        Collections.shuffle(tileList); // randomizes tiles
         return tileList;
     }
 
-    // method to arrange the tile list into a 4x3 grid
-    // use a 2d array for dashes and a corresponding one for the tiles
 
-
-    public void runGame() {
-        Scanner scanner = new Scanner(System.in);
+    public String[][] setUpDashBoard() {
         // sets up a matrix of dashes to be initially displayed in the console
-        final String[][] dashBoard = {{"-", "-", "-", "-"}, {"-", "-", "-", "-"}, {"-", "-", "-", "-"}};
+        String[][] dashBoard = {{"-", "-", "-", "-"}, {"-", "-", "-", "-"}, {"-", "-", "-", "-"}};
+        return dashBoard;
+    }
 
+    public String[][] setUpKeyBoard() {
         // setting up a matrix of the keys of the Tile objects in the randomized arrayList
-        int[][] keyBoard = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}; // sets each value in the 2d array to 0
+        String[][] keyBoard = setUpDashBoard(); // make a board which will eventually contain the keys
         int arrayListIndex = 0; // this counts the indexes of arrayList
-        ArrayList<Tile> tileList;
-        tileList = createTileList(); // create randomized list of Tile objects
+        ArrayList<Tile> tileList = createTileList(); // create randomized list of Tile objects
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 4; j++) {
                 // sets the value of the matrix in the current position to the key of the corresponding tile object
-                keyBoard[i][j] = tileList.get(arrayListIndex).getKey();
+                keyBoard[i][j] = String.valueOf(tileList.get(arrayListIndex).getKey());
                 arrayListIndex++; // increases ArrayList index by 1
             }
         }
+        return keyBoard;
+    }
 
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.println(dashBoard[i][j] + " ");
-            }
-            System.out.println();
-        }
+    public void runGame() {
+        String[][] baseBoard = setUpDashBoard(); // this board will track the user's correct matches
+        final String[][] keyBoard = setUpKeyBoard(); // the board representation of the arrayList of Tile keys
+        String flexBoard[][] = setUpDashBoard(); // the board which will be changed when user's first choice is printed
+        Scanner scanner = new Scanner(System.in); // scanner
+
+        printBoard(baseBoard);
+
+        // getting input from first user choice
         System.out.println("Please enter a number from 1-12 for the tile you want to flip.");
         System.out.println("The numbers go from left->right and then top->bottom");
         String input_1 = scanner.nextLine();
-        // change the tile which corresponds to input from a - to its numerical key
+
+        // TO-DO: change the tile which corresponds to input from a - to its numerical key
+
+        // getting input from second user choice
         System.out.println("Please enter another number from 1-12 for the tile you want to flip.");
         System.out.println("The numbers go from left->right and then top->bottom");
         String input_2 = scanner.nextLine();
+
         // if the two inputs match, leave them as numerical keys
         // if they do not match, turn them both back to dashes
 
+    }
+
+    public void printBoard(String[][] board) {
+        // prints dashboard
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main (String [] args) {
