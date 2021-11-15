6e3fc6355361715ea6ada1c672330849b5b3691e
iwisvirus <gaonasi6262@gmail.com>
2021-11-14 21:32:52 -0500

    Implemented exceptions for UserGameInput class.

--- src/main/Controller/UserGameInput.java
+++ src/main/Controller/UserGameInput.java
@@ -1,32 +1,73 @@
 package Controller;
 
-import Entity.Tile;
-import Entity.TileBoard;
-import UseCase.BoardGenerator;
-
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class UserGameInput {
 
-    public static int[] getUserMove() {
+    public static int[] getUserMove(int numRows, int numCols) {
         //TODO implement exception for invalid inputs
         //Store move data in an array [row number, column number]
         int[] input = new int[2];
         //Get user's move and store in input
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter row number: ");
-        int rowNum = Integer.parseInt(scanner.nextLine());
-        System.out.println("Enter column number: ");
-        int colNum = Integer.parseInt(scanner.nextLine());
-        input[0] = rowNum;
-        input[1] = colNum;
+        boolean done = false;
+        while (!done){
+            System.out.println("Enter row number: ");
+            try {
+                int n = scanner.nextInt();
+                if (n <= numRows && n > 0){
+                    done = true;
+                    input[0] = n;
+                }
+            }
+            catch (InputMismatchException e){
+                scanner.nextLine();
+            }
+        }
+        boolean check = false;
+        while (!check){
+            System.out.println("Enter column number: ");
+            try {
+                int n = scanner.nextInt();
+                if (n <= numCols && n > 0){
+                    check = true;
+                    input[1] = n;
+                }
+            }
+            catch (InputMismatchException e){
+                scanner.nextLine();
+            }
+        }
+
         return input;
     }
 
     public static int getUserDifficulty() {
+        int input = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Difficulty (1-3): ");
         //TODO: Implement exception for invalid input
-        return Integer.parseInt(scanner.nextLine());
+        boolean done = false;
+        while (!done){
+            System.out.println("Enter Difficulty (1-3): ");
+            try {
+                int n = scanner.nextInt();
+                if (n <= 3 && n>0){
+                    done = true;
+                    input = input + n;
+                }
+            }
+            catch (InputMismatchException e){
+                scanner.nextLine();
+            }
+
+        }
+        return input;
+
+        //return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static void main(String[] args) {
+        int [] a = getUserMove(3,4);
     }
 }
