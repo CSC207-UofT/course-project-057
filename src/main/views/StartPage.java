f08daf897509a623a740501e8783889ae48096a8
akansha313 <aka.singh@mail.utoronto.ca>
2021-11-29 22:08:37 -0500

    Fixed errors in DisplayPrompts, updated MatchingGame and PatternGame to include StartPage, updated the testable (no SQL) method in main, added javadoc (Akansha)

--- src/main/views/StartPage.java
+++ src/main/views/StartPage.java
@@ -3,6 +3,7 @@ package views;
 public class StartPage {
 
     public static String[] startPage() {
+        DisplayPrompts.welcomeMessage();
         String[] gameType = new String[3];
         gameType[0] = UserGameInput.getGameType();
         gameType[1] = UserGameInput.getUserDifficulty();
