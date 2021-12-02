c23983772b9f3c3fd3e23b0378af75e2331502f9
Wei Dong <addmoosy@gmail.com>
2021-12-01 21:37:20 -0500

    Update MatchingGame GUI (Wei, Iris, Koji)

--- src/main/views/MatchingGame.java
+++ src/main/views/MatchingGame.java
@@ -9,6 +9,8 @@ import usecase.BoardManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -19,12 +21,14 @@ import java.sql.SQLException;
  */
 
 public class MatchingGame {
-    JFrame frame;
-    JPanel panel;
-    JLabel title, time, totalMove;
-    JButton setting;
-    JLabel[][] tiles;
-    Font f1, f2;
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel title, time, totalMove;
+    private JButton setting;
+    private JLabel[][] tiles;
+    private Font f1, f2;
+    private static MatchingBoard board;
+    int rowNum,colNum;
 
     /**
      *
@@ -43,12 +47,42 @@ public class MatchingGame {
         f2 = new Font(title.getFont().getName(), Font.PLAIN, 15);//paragraph font
         tiles = new JLabel[ DifficultyStrategy.valueOf(difficulty).setDimension()[0]]
                 [ DifficultyStrategy.valueOf(difficulty).setDimension()[1]];
-
+        board = DifficultyStrategy.valueOf(difficulty).generateMatchingBoard();
 
         //setup panel
         panel.setLayout(null);
         panel.setBounds(0,0,960,540);
         panel.setBackground(Color.GRAY);
+        panel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                colNum = (int) Math.floor((e.getX()-40)/100.0);
+                rowNum = (int) Math.floor((e.getY()-100)/60.0);
+                if ((colNum >= 0 && colNum <= tiles.length)&&(rowNum >= 0 && rowNum <= tiles[0].length)) {
+                    JOptionPane.showMessageDialog(new JFrame(), colNum + ", " + rowNum);
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
 
         //setup labels
         title.setBounds(320,30,300,50);
@@ -63,6 +97,7 @@ public class MatchingGame {
                 tiles[i][j] = new JLabel("label " + i + "-"+ j);
                 tiles[i][j].setBounds(40+100*i,100+60*j,100,60);
                 tiles[i][j].setFont(f2);
+                tiles[i][j].setBorder(BorderFactory.createLineBorder(Color.green, 2));
                 panel.add(tiles[i][j]);
             }
         }
@@ -76,6 +111,7 @@ public class MatchingGame {
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
     }
     /**
      * runs a new game mode
@@ -88,8 +124,8 @@ public class MatchingGame {
         // String difficulty = UserGameInput.getUserDifficulty(); // delete, this is moved to StartPage
         int numMoves = 0;
 
-        MatchingBoard board = DifficultyStrategy.valueOf(difficulty).generateMatchingBoard();
 
+        //MatchingBoard board = DifficultyStrategy.valueOf(difficulty).generateMatchingBoard();
         // System.out.println("Input a row number from 1 to " + board.getNumRows()
         //        + " and a column from 1 to " + (board.getNumCols()) + ". Tile must not be revealed.");
         DisplayPrompts.enterMoveDisplay(board.getNumRows(), board.getNumCols());
@@ -102,6 +138,7 @@ public class MatchingGame {
             int[] move2 = BoardManager.Move(board, "Matching", 0);
             int move1Key = board.getTileKey(move1[0], move1[1]);
             int move2Key = board.getTileKey(move2[0], move2[1]);
+
             DisplayPrompts.matchDisplay(move1Key == move2Key);
             if (!(move1Key == move2Key)) {
                 BoardManager.flipTile(board, move1[0], move1[1]);
@@ -171,5 +208,21 @@ public class MatchingGame {
             System.out.println("Time: " + time);
             System.out.println("Difficulty: " + difficulty);
         }
+    }
+
+    /**
+     * rowNum Getter
+     * @return rowNum
+     */
+    public int getRowNum(){
+        return rowNum;
+    }
+
+    /**
+     * colNum Getter
+     * @return colNum
+     */
+    public int getColNum(){
+      return colNum;
     }
 }
