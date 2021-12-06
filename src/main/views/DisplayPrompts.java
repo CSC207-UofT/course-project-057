07886fde4c27115210f997b79145fc342e0709f8
akansha313 <aka.singh@mail.utoronto.ca>
2021-12-05 21:16:40 -0500

    Added matchingRules and patternRules to displayPrompts (Akansha)

--- src/main/views/DisplayPrompts.java
+++ src/main/views/DisplayPrompts.java
@@ -115,4 +115,25 @@ public class DisplayPrompts {
         return("Would you like to play as a guest? (Y/N)");
     }
 
+    public static String matchingRules() {
+        return ("Matching Rules: " +
+                "1. Your aim is to uncover every pair of tiles. Each tile in the game has another matching tile." +
+                "2. Uncover a tile by clicking on it." +
+                "3. Then, press another tile. " +
+                "4. If the two tiles look the same, you have found a pair!" +
+                "5. If the two tiles are not the same, they will flip back over and you can try again by flipping another tile." +
+                "6. The game ends when all tiles have been uncovered. Good luck and thanks for playing!");
+    }
+
+    public static String patternRules() {
+        return ("Pattern Rules: " +
+                "1. Your aim is to copy the pattern shown on screen." +
+                "2. One tile will uncover. Remember this tile!" +
+                "3. Press the same tile that was just shown to you! " +
+                "4. Another tile will flip." +
+                "5. Press the tile shown in step 2, followed by the tile in step 4." +
+                "6. The next tile will flip. Keep repeating the same pattern if you can remember!" +
+                "7. Good luck and thanks for playing!");
+    }
+
 }
